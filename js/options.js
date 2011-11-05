2a1e2e32f2405b6b796a71fab9591b338daa891c
Vadim Frolov <fralik@gmail.com>
2011-11-06 01:43:45 +0400

    Added copyright header to js files

--- js/options.js
+++ js/options.js
@@ -1,4 +1,23 @@
-﻿window.addEventListener("load", init, false);
+﻿/**
+ * Copyright (c) Vadim Frolov, 2011.
+ * Contact: fralik@gmail.com
+ *
+ * GNU General Public License Usage
+ * This file may be used under the terms of the GNU General
+ * Public License version 3.0 as published by the Free Software Foundation
+ * and appearing in the file LICENSE.GPL included in the packaging of this
+ * file. Please review the following information to ensure the GNU General
+ * Public License version 3.0 requirements will be met:
+ * http://www.gnu.org/copyleft/gpl.html.
+ *
+ * Other Usage
+ * Alternatively, this file may be used in accordance with the terms and
+ * conditions contained in a signed written agreement between you and Vadim Frolov.
+ *
+ * This file provides objects and function that should be shared between all components of the extension.
+ */
+
+window.addEventListener("load", init, false);
 
 function init() {
     // <-- flattr code
